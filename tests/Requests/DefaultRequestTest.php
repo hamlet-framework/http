dfc531afb25c21dc7372fa88dd5102e5e7afecef
Vasily Kartashov <vasily.kartashov@hunterindustries.com>
2019-05-14 16:20:37 +1000

    adding tests for server param reader, adding lint

--- tests/Requests/DefaultRequestTest.php
+++ tests/Requests/DefaultRequestTest.php
@@ -245,4 +245,113 @@ class DefaultRequestTest extends TestCase
 
         $request->getBodyParam('id', _class(\DateTime::class));
     }
+
+    public function server_params()
+    {
+        return [
+            [
+                [
+                    'USER' => 'nginx',
+                    'HOME' => '/var/lib/nginx',
+                    'HTTP_CONNECTION' => 'keep-alive',
+                    'HTTP_X_FORWARDED_PROTO' => 'http',
+                    'HTTP_X_FORWARDED_PORT' => '80',
+                    'HTTP_X_FORWARDED_FOR' => '11.14.6.2',
+                    'HTTP_USER_AGENT' => 'Mozilla/5.0',
+                    'HTTP_UPGRADE_INSECURE_REQUESTS' => '1',
+                    'HTTP_DNT' => '1',
+                    'HTTP_COOKIE' => 'PHPSESSID=uebp1a6lmdjcvm7hhhcbm27ra7',
+                    'HTTP_CACHE_CONTROL' => 'max-age=0',
+                    'HTTP_ACCEPT_LANGUAGE' => 'en,ru;q=0.9,es;q=0.8,de;q=0.7',
+                    'HTTP_ACCEPT_ENCODING' => 'gzip, deflate',
+                    'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                    'HTTP_HOST' => 'example.com',
+                    'REDIRECT_STATUS' => '200',
+                    'SERVER_NAME' => '',
+                    'SERVER_PORT' => '80',
+                    'SERVER_ADDR' => '10.0.2.53',
+                    'REMOTE_PORT' => '1960',
+                    'REMOTE_ADDR' => '10.0.2.122',
+                    'SERVER_SOFTWARE' => 'nginx/1.14.1',
+                    'GATEWAY_INTERFACE' => 'CGI/1.1',
+                    'REQUEST_SCHEME' => 'http',
+                    'SERVER_PROTOCOL' => 'HTTP/1.1',
+                    'DOCUMENT_ROOT' => '/var/www/html/html',
+                    'DOCUMENT_URI' => '/test.php',
+                    'REQUEST_URI' => '/test.php?a=12',
+                    'SCRIPT_NAME' => '/test.php',
+                    'CONTENT_LENGTH' => '',
+                    'CONTENT_TYPE' => '',
+                    'REQUEST_METHOD' => 'GET',
+                    'QUERY_STRING' => 'a=12',
+                    'SCRIPT_FILENAME' => '/var/www/test.php',
+                    'FCGI_ROLE' => 'RESPONDER',
+                    'PHP_SELF' => '/test.php',
+                    'REQUEST_TIME_FLOAT' => '1557814144.2578',
+                    'REQUEST_TIME' => '1557814144',
+                ],
+                'http://example.com/test.php?a=12'
+            ],
+            [
+                [
+                    'USER' => 'nginx',
+                    'HOME' => '/var/lib/nginx',
+                    'HTTP_CONNECTION' => 'keep-alive',
+                    'HTTP_X_FORWARDED_PROTO' => 'https',
+                    'HTTP_X_FORWARDED_PORT' => '443',
+                    'HTTP_X_FORWARDED_FOR' => '11.14.6.2',
+                    'HTTP_USER_AGENT' => 'Mozilla/5.0',
+                    'HTTP_UPGRADE_INSECURE_REQUESTS' => '1',
+                    'HTTP_DNT' => '1',
+                    'HTTP_COOKIE' => 'PHPSESSID=uebp1a6lmdjcvm7hhhcbm27ra7;',
+                    'HTTP_CACHE_CONTROL' => 'max-age=0',
+                    'HTTP_ACCEPT_LANGUAGE' => 'en,ru;q=0.9,es;q=0.8,de;q=0.7',
+                    'HTTP_ACCEPT_ENCODING' => 'gzip, deflate, br',
+                    'HTTP_ACCEPT' => 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3',
+                    'HTTP_HOST' => 'example.com',
+                    'REDIRECT_STATUS' => '200',
+                    'SERVER_NAME' => '',
+                    'SERVER_PORT' => '80',
+                    'SERVER_ADDR' => '10.0.2.53',
+                    'REMOTE_PORT' => '44580',
+                    'REMOTE_ADDR' => '10.0.1.104',
+                    'SERVER_SOFTWARE' => 'nginx/1.14.1',
+                    'GATEWAY_INTERFACE' => 'CGI/1.1',
+                    'REQUEST_SCHEME' => 'http',
+                    'SERVER_PROTOCOL' => 'HTTP/1.1',
+                    'DOCUMENT_ROOT' => '/var/www/html/html',
+                    'DOCUMENT_URI' => '/test.php',
+                    'REQUEST_URI' => '/test.php?a=12',
+                    'SCRIPT_NAME' => '/test.php',
+                    'CONTENT_LENGTH' => '',
+                    'CONTENT_TYPE' => '',
+                    'REQUEST_METHOD' => 'GET',
+                    'QUERY_STRING' => 'a=12',
+                    'SCRIPT_FILENAME' => '/var/www/html/html/test.php',
+                    'FCGI_ROLE' => 'RESPONDER',
+                    'PHP_SELF' => '/test.php',
+                    'REQUEST_TIME_FLOAT' => '1557814388.9157',
+                    'REQUEST_TIME' => '1557814388'
+                ],
+                'https://example.com/test.php?a=12'
+            ]
+        ];
+    }
+
+    /**
+     * @dataProvider server_params()
+     * @param array $serverParams
+     * @param string $uri
+     */
+    public function test_get_uri_from_server_params(array $serverParams, string $uri)
+    {
+        $proxy = new class() extends DefaultRequest
+        {
+            public function convert(array $serverParams): UriInterface
+            {
+                return parent::readUriFromServerParams($serverParams);
+            }
+        };
+        Assert::assertEquals($uri, (string) $proxy->convert($serverParams));
+    }
 }
